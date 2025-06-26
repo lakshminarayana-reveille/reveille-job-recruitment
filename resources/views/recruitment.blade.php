3f429eb0cdcb2f134a9b3ee44802c148c36d4b0a
Lakshmi Narayana <narayanasekhar@reveilletechnologies.com>
2025-06-26 10:49:09 +0530

    Remove job application functionality and consolidate to recruitment system

--- resources/views/recruitment.blade.php
+++ resources/views/recruitment.blade.php
@@ -20,7 +20,7 @@
                 {{ session('success') }}
             </div>
         @endif
-        @if ($errors->any())
+        @if ($errors->any() && session('current_step', 1) === 7)
             <div class="mb-4 p-4 bg-red-100 text-red-700 rounded-md">
                 <ul>
                     @foreach ($errors->all() as $error)
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\JobApplicationController;
 use App\Http\Controllers\RecruitmentController;
 use Illuminate\Support\Facades\Route;
 
@@ -13,10 +12,3 @@ Route::get('/recruitment', [RecruitmentController::class, 'showForm'])->name('re
 Route::post('/recruitment/step/{step}', [RecruitmentController::class, 'storeStep'])->name('recruitment.store-step');
 Route::post('/recruitment/submit', [RecruitmentController::class, 'submit'])->name('recruitment.submit');
 Route::post('/recruitment/reset', [RecruitmentController::class, 'reset'])->name('recruitment.reset');
-
-
-Route::get('/job-application', [JobApplicationController::class, 'showForm'])->name('job-application.show');
-Route::post('/job-application/step/{step}', [JobApplicationController::class, 'storeStep'])->name('job-application.store-step');
-Route::post('/job-application/submit', [JobApplicationController::class, 'submit'])->name('job-application.submit');
-Route::post('/job-application/save-draft', [JobApplicationController::class, 'saveDraft'])->name('job-application.save-draft');
-Route::post('/job-application/reset', [JobApplicationController::class, 'reset'])->name('job-application.reset');
